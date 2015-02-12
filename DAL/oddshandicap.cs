98fcbdc2812981c555bd3fef573c211de39bf9e4
kaleo <kaleo1990@hotmail.com>
2015-02-12 16:11:32 +0800

    上传

---
+++ DAL/oddshandicap.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Data;
+using System.Text;
+using System.Data.OleDb;
+using Maticsoft.DBUtility;//Please add references
+namespace kaleosoft.DAL
+{
+	/// <summary>
+	/// 数据访问类:oddshandicap
+	/// </summary>
+	public partial class oddshandicap
+	{
+		public oddshandicap()
+		{}
+		#region  BasicMethod
+
+
+
+		/// <summary>
+		/// 增加一条数据
+		/// </summary>
+		public bool Add(kaleosoft.Model.oddshandicap model)
+		{
+			StringBuilder strSql=new StringBuilder();
+			strSql.Append("insert into oddshandicap(");
+			strSql.Append("name)");
+			strSql.Append(" values (");
+			strSql.Append("@name)");
+			OleDbParameter[] parameters = {
+					new OleDbParameter("@name", OleDbType.VarChar,0)};
+			parameters[0].Value = model.name;
+
+			int rows=DbHelperOleDb.ExecuteSql(strSql.ToString(),parameters);
+			if (rows > 0)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		/// <summary>
+		/// 更新一条数据
+		/// </summary>
+		public bool Update(kaleosoft.Model.oddshandicap model)
+		{
+			StringBuilder strSql=new StringBuilder();
+			strSql.Append("update oddshandicap set ");
+			strSql.Append("name=@name");
+			strSql.Append(" where ");
+			OleDbParameter[] parameters = {
+					new OleDbParameter("@name", OleDbType.VarChar,0),
+					new OleDbParameter("@id", OleDbType.Integer,4)};
+			parameters[0].Value = model.name;
+			parameters[1].Value = model.id;
+
+			int rows=DbHelperOleDb.ExecuteSql(strSql.ToString(),parameters);
+			if (rows > 0)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+		/// <summary>
+		/// 删除一条数据
+		/// </summary>
+		public bool Delete()
+		{
+			//该表无主键信息，请自定义主键/条件字段
+			StringBuilder strSql=new StringBuilder();
+			strSql.Append("delete from oddshandicap ");
+			strSql.Append(" where ");
+			OleDbParameter[] parameters = {
+			};
+
+			int rows=DbHelperOleDb.ExecuteSql(strSql.ToString(),parameters);
+			if (rows > 0)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+
+		/// <summary>
+		/// 得到一个对象实体
+		/// </summary>
+		public kaleosoft.Model.oddshandicap GetModel()
+		{
+			//该表无主键信息，请自定义主键/条件字段
+			StringBuilder strSql=new StringBuilder();
+			strSql.Append("select id,name from oddshandicap ");
+			strSql.Append(" where ");
+			OleDbParameter[] parameters = {
+			};
+
+			kaleosoft.Model.oddshandicap model=new kaleosoft.Model.oddshandicap();
+			DataSet ds=DbHelperOleDb.Query(strSql.ToString(),parameters);
+			if(ds.Tables[0].Rows.Count>0)
+			{
+				return DataRowToModel(ds.Tables[0].Rows[0]);
+			}
+			else
+			{
+				return null;
+			}
+		}
+
+
+		/// <summary>
+		/// 得到一个对象实体
+		/// </summary>
+		public kaleosoft.Model.oddshandicap DataRowToModel(DataRow row)
+		{
+			kaleosoft.Model.oddshandicap model=new kaleosoft.Model.oddshandicap();
+			if (row != null)
+			{
+				if(row["id"]!=null && row["id"].ToString()!="")
+				{
+					model.id=int.Parse(row["id"].ToString());
+				}
+				if(row["name"]!=null)
+				{
+					model.name=row["name"].ToString();
+				}
+			}
+			return model;
+		}
+
+		/// <summary>
+		/// 获得数据列表
+		/// </summary>
+		public DataSet GetList(string strWhere)
+		{
+			StringBuilder strSql=new StringBuilder();
+			strSql.Append("select id,name ");
+			strSql.Append(" FROM oddshandicap ");
+			if(strWhere.Trim()!="")
+			{
+				strSql.Append(" where "+strWhere);
+			}
+			return DbHelperOleDb.Query(strSql.ToString());
+		}
+
+		/// <summary>
+		/// 获取记录总数
+		/// </summary>
+		public int GetRecordCount(string strWhere)
+		{
+			StringBuilder strSql=new StringBuilder();
+			strSql.Append("select count(1) FROM oddshandicap ");
+			if(strWhere.Trim()!="")
+			{
+				strSql.Append(" where "+strWhere);
+			}
+			object obj = DbHelperSQL.GetSingle(strSql.ToString());
+			if (obj == null)
+			{
+				return 0;
+			}
+			else
+			{
+				return Convert.ToInt32(obj);
+			}
+		}
+		/// <summary>
+		/// 分页获取数据列表
+		/// </summary>
+		public DataSet GetListByPage(string strWhere, string orderby, int startIndex, int endIndex)
+		{
+			StringBuilder strSql=new StringBuilder();
+			strSql.Append("SELECT * FROM ( ");
+			strSql.Append(" SELECT ROW_NUMBER() OVER (");
+			if (!string.IsNullOrEmpty(orderby.Trim()))
+			{
+				strSql.Append("order by T." + orderby );
+			}
+			else
+			{
+				strSql.Append("order by T.id desc");
+			}
+			strSql.Append(")AS Row, T.*  from oddshandicap T ");
+			if (!string.IsNullOrEmpty(strWhere.Trim()))
+			{
+				strSql.Append(" WHERE " + strWhere);
+			}
+			strSql.Append(" ) TT");
+			strSql.AppendFormat(" WHERE TT.Row between {0} and {1}", startIndex, endIndex);
+			return DbHelperOleDb.Query(strSql.ToString());
+		}
+
+		/*
+		/// <summary>
+		/// 分页获取数据列表
+		/// </summary>
+		public DataSet GetList(int PageSize,int PageIndex,string strWhere)
+		{
+			OleDbParameter[] parameters = {
+					new OleDbParameter("@tblName", OleDbType.VarChar, 255),
+					new OleDbParameter("@fldName", OleDbType.VarChar, 255),
+					new OleDbParameter("@PageSize", OleDbType.Integer),
+					new OleDbParameter("@PageIndex", OleDbType.Integer),
+					new OleDbParameter("@IsReCount", OleDbType.Boolean),
+					new OleDbParameter("@OrderType", OleDbType.Boolean),
+					new OleDbParameter("@strWhere", OleDbType.VarChar,1000),
+					};
+			parameters[0].Value = "oddshandicap";
+			parameters[1].Value = "id";
+			parameters[2].Value = PageSize;
+			parameters[3].Value = PageIndex;
+			parameters[4].Value = 0;
+			parameters[5].Value = 0;
+			parameters[6].Value = strWhere;	
+			return DbHelperOleDb.RunProcedure("UP_GetRecordByPage",parameters,"ds");
+		}*/
+
+		#endregion  BasicMethod
+		#region  ExtensionMethod
+
+		#endregion  ExtensionMethod
+	}
+}
+
